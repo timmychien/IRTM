0c489864e054b993ec6da06ba5e7f6b6aa33c3a0
timmychien <46017390+timmychien@users.noreply.github.com>
2021-12-03 22:06:34 +0800

    Add files via upload

--- pa2.py
+++ pa2.py
@@ -5,6 +5,7 @@ import ssl
 import os
 import math
 import numpy as np
+import collections
 ssl._create_default_https_context = ssl._create_unverified_context
 def tokenize(article):
     article=article.lower()
@@ -30,34 +31,44 @@ def stopwords(stemlist):
     return result
 
 def create_dictionary(documents):
-    dictionary={}
+    diction={}
+    dictionary=[]
     for doc in documents:
-        for word in doc.split(' '):
-            if word not in dictionary:
-                dictionary[word]=1
-            else:dictionary[word]+=1
+        for word in set(doc.split(' ')):
+            if word not in diction:
+                diction[word]=1
+            else:diction[word]+=1
+    items=diction.items()
+    items=sorted(items)
+    for i in range(0,len(diction)):
+        dictionary.append([i+1,items[i]])
     return dictionary
-
 def tfidf(docId):
     global dictionary
     global documents
     document=documents[docId-1]
     idf={}
     tf={}
+    tf_idx={}
     tfidf={}
     for word in document.split(' '):
-        if word not in tf:
-            tf[word]=1
-        else:tf[word]+=1
-    for word in dictionary:
-        idf[word]=math.log10(1095/int(dictionary[word]))
+        for i in range(len(dictionary)):
+            if word==dictionary[i][1][0]:
+                if word not in tf:
+                    tf[word]=1
+                else:tf[word]+=1
     for word in tf:
-        tfidf[word]=int(tf[word])*idf[word]
+        for i in range(len(dictionary)):
+            if word==dictionary[i][1][0]:
+                tf_idx[dictionary[i][0]]=tf[word]
+    for i in range(len(dictionary)):
+        idf[dictionary[i][0]]=math.log10(1095/int(dictionary[i][1][1]))
+    for idx in tf_idx:
+        tfidf[idx]=(int(tf_idx[idx])*idf[idx])/len(tf_idx)
+    tfidf=collections.OrderedDict(sorted(tfidf.items()))
     return tfidf
 def cosine(Docx,Docy):
     global dictionary
-    s1=0
-    s2=0
     tfidf1=tfidf(Docx)
     tfidf2=tfidf(Docy)
     diff1=tfidf1.keys()-tfidf2.keys()
@@ -71,22 +82,16 @@ def cosine(Docx,Docy):
     tfidf1_items=sorted(tfidf1_items)
     tfidf2_items = tfidf2.items()
     tfidf2_items=sorted(tfidf2_items)
-    for i in range(len(tfidf1)):
-        s1+=(tfidf1_items[i][1]**2)
-    s1=math.sqrt(s1)
-    for j in range(len(tfidf2)):
-        s2+=(tfidf2_items[j][1]**2)
-    s2=math.sqrt(s2)
     v1=[tfidf1_items[i][1] for i in range(len(tfidf1_items))]
     v2=[tfidf2_items[j][1] for j in range(len(tfidf2_items))]
     v1=np.array(v1)
     v2=np.array(v2)
-    sim=(v1.dot(v2))/(s1*s2)
+    sim=(v1.dot(v2))
     return sim
-documents=[]
 
+documents=[]
 #preprocessing
-'''
+
 all_files=os.listdir('./IRTM')
 for file in all_files:
     f=open('./IRTM/'+file,'r').read()
@@ -96,42 +101,40 @@ for file in all_files:
     output=open('./data/'+file,'wb')
     output.write(result.encode())
 
-'''
 all_files_stemmed=os.listdir('./data')
 for file in all_files_stemmed:
     f=open('./data/'+file,'r').read()
     documents.append(f)
-
 #1.create dictionary.txt
-
 dictionary=create_dictionary(documents)
-items = dictionary.items()
-items=sorted(items)
 dictionary_output=open('dictionary.txt','wb')
 dictionary_output.write(f't_index\tterm\tdf\n'.encode())
-for i in range(1,len(dictionary)):
-    dictionary_output.write(f'{i}\t{items[i][0]:^}\t{items[i][1]:^}\n'.encode())
+for i in range(len(dictionary)):
+    dictionary_output.write(f'{dictionary[i][0]}\t{dictionary[i][1][0]:^}\t{dictionary[i][1][1]:^}\n'.encode())
 dictionary_output.close()
-#2.tf-idf 1.txt
 
+#2.tf-idf 1.txt
 tfidf1=tfidf(1)
-tfidf1_items = tfidf1.items()
-tfidf1_items=sorted(tfidf1_items)
-tfidf1_output=open('1.txt','wb')
-tfidf1_output.write(f't_index\tterm\ttf-idf\n'.encode())
+tfidf1=list(tfidf1.items())
+tfidf1_output=open('doc1.txt','wb')
+tfidf1_output.write(f'amount of term:\n'.encode())
+tfidf1_output.write(f'{len(tfidf1)}\n'.encode())
+tfidf1_output.write(f't_index\ttf-idf\n'.encode())
 for i in range(len(tfidf1)):
-    tfidf1_output.write(f'{i+1}\t{tfidf1_items[i][0]:^}\t{tfidf1_items[i][1]:^}\n'.encode())
+    tfidf1_output.write(f'{tfidf1[i][0]}\t{tfidf1[i][1]:^}\n'.encode())
 tfidf1_output.close()
+
 # tfidf of all documents
+'''
 for idx in range(len(documents)):
     tfidf_doc=tfidf(i)
-    tfidf_items = tfidf_doc.items()
-    tfidf_items=sorted(tfidf_items)
+    tfidf_doc=list(tfidf_doc.items())
     tfidf_output=open(f'./output/{idx+1}.txt','wb')
-    tfidf_output.write(f't_index\tterm\ttf-idf\n'.encode())
+    tfidf_output.write(f't_index\ttf-idf\n'.encode())
     for i in range(len(tfidf_doc)):
-        tfidf_output.write(f'{i+1}\t{tfidf_items[i][0]:^}\t{tfidf_items[i][1]:^}\n'.encode())
+        tfidf_output.write(f'{tfidf_doc[i][0]}\t{tfidf_doc[i][1]}\n'.encode())
     tfidf_output.close()
+'''
 #3.calculate cosine simularity
 cosine1_2=cosine(1,2)
 print('cosine similaritiy of Doc1 and Doc2:',cosine1_2)
